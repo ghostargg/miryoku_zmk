8d738fad787a18f4517a94acc9227785a29cad7b
Paweł Smoliński <smolson1337@gmail.com>
2024-11-28 18:38:18 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,8 +4,8 @@
 #define XXX &none
 
 #define MIRYOKU_LAYER_FUN \
-&kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         &u_to_U_WOW,       &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          &u_to_U_VAL,       &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         &mo U_WOW,         &mo U_BASE,        &mo U_EXTRA,       &mo U_TAP,         U_BOOT,            \
+&kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          &mo U_VAL,         &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              U_NA,              U_NA,              &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp TAB,           &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
@@ -13,10 +13,10 @@ U_NP,              U_NP,              &kp K_APP,         &kp TAB,           &kp 
 &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LCTRL,         &kp Z,             &kp X,             &kp C,             U_MT(N4, V),       U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-U_NP,              U_NP,              U_LT(U_VTWO, ESC), &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
+U_NP,              U_NP,              &mo U_VTWO,        &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
 
 #define MIRYOKU_LAYER_WOW \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &u_to_U_WOW,       &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,                \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,          \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_MT(ESC, Z),      &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              &kp LSHIFT, &kp LCTRL,U_LT(U_FUN, SPACE) ,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
@@ -38,8 +38,8 @@ K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
 N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_VAL\
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &u_to_U_WOW \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
@@ -56,12 +56,33 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(VAL,   "Valo1") \
+MIRYOKU_X(VAL,    "Valo1") \
 MIRYOKU_X(VTWO,   "Valo2") \
-MIRYOKU_X(WOW,   "Wow")
+MIRYOKU_X(WOW,    "Wow")
+
+#define MIRYOKU_LAYERMAPPING_VAL( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_BASE \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_WOW( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_BASE \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_VAL MIRYOKU_MAPPING
-#define MIRYOKU_LAYERMAPPING_VTWO MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_WOW MIRYOKU_MAPPING
 
 #define U_BASE   0
