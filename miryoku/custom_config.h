ab5eb5e336525523c8506cdb568ddd64b75156f3
Paweł Smoliński <smolson1337@gmail.com>
2025-01-27 23:00:11 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -31,15 +31,14 @@ U_NP,              U_NP,              U_NA,              &kp LCTRL,         &kp 
 
 
 
-// Direct custom layer access
-#define MIRYOKU_LAYOUTMAPPING_CORNE(\
-K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYERMAPPING_BASE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_VAL\
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &u_to_U_WOW \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_WOW \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
@@ -56,8 +55,8 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(VAL,    "Valo1") \
-MIRYOKU_X(VTWO,   "Valo2") \
+MIRYOKU_X(VAL,    "Valoone") \
+MIRYOKU_X(VTWO,   "Valotwo") \
 MIRYOKU_X(WOW,    "Wow")
 
 #define MIRYOKU_LAYERMAPPING_VAL( \
@@ -82,8 +81,8 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYERMAPPING_VAL MIRYOKU_MAPPING
-#define MIRYOKU_LAYERMAPPING_WOW MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_VTWO MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_FUN MIRYOKU_MAPPING
 
 #define U_BASE   0
 #define U_EXTRA  1
